e744850c2cff67ee3585a0e72990ff435b96e93c
chris correia <chriscorreia2001@gmail.com>
2022-07-25 11:00:46 +0530

    button

--- chat_app/urls.py
+++ chat_app/urls.py
@@ -4,7 +4,7 @@ from django.urls import path
 
 urlpatterns = [
     path('go_to/',go_toAPIView,name="got_to"),
-    path('carousal/',carousalAPIView,name="carousal"),
+    path('carousel/',carousalAPIView,name="carousel"),
     path('video/',videoAPIView,name="video"),
     path('button/',buttonAPIView,name="button"),
     path('send_message/',sendmessageAPIView,name="send message"),
