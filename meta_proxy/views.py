fdfd8ac5f60aed206c9c70a46e214d00ed3bd6d5
chris correia <chriscorreia2001@gmail.com>
2022-10-20 10:59:54 +0530

    save

--- meta_proxy/views.py
+++ meta_proxy/views.py
@@ -2,12 +2,11 @@ from django.shortcuts import render
 from urllib.parse import urlparse
 from django.http import JsonResponse
 from  rest_framework.decorators import *
-import metadata_parser
+from metadata_parser import MetadataParser
 
 @api_view(["GET"])
 def metaProxy(request):
     url = request.GET["url"]
-    page = metadata_parser.MetadataParser(url,search_head_only=True)
     metaJson = {
     "description": "",
     "image": "",
@@ -15,21 +14,27 @@ def metaProxy(request):
     "url": url,
     "domain":getDomain(url)
     }
-    # print(page.metadata)
-    if page.metadata.get("og",{}).get("image"):
-        image_url = page.metadata.get("og",{}).get("image")
-        if is_url(image_url):
-            metaJson["image"]=page.metadata.get("og",{}).get("image")
-        else:
-            metaJson["image"]=page.metadata.get("og",{}).get("url","")+page.metadata.get("og",{}).get("image")
-    if page.metadata.get("og",{}).get("title"):
-        metaJson["title"]=page.metadata.get("og",{}).get("title")
-    if page.metadata.get("page",{}).get("title"):
-        metaJson["title"]=page.metadata.get("page",{}).get("title")
-    if page.metadata.get("og",{}).get("description"):
-        metaJson["description"]=page.metadata.get("og",{}).get("description")
-    if page.metadata.get("meta",{}).get("description"):
-        metaJson["description"]=page.metadata.get("meta",{}).get("description")
+    if(is_url(url)):
+        try:
+            page = MetadataParser(url,search_head_only=True,force_doctype=True,support_malformed=True,force_parse=True)
+            
+            # print(page.metadata)
+            if page.metadata.get("og",{}).get("image"):
+                image_url = page.metadata.get("og",{}).get("image")
+                if is_url(image_url):
+                    metaJson["image"]=page.metadata.get("og",{}).get("image")
+                else:
+                    metaJson["image"]=page.metadata.get("og",{}).get("url","")+page.metadata.get("og",{}).get("image")
+            if page.metadata.get("og",{}).get("title"):
+                metaJson["title"]=page.metadata.get("og",{}).get("title")
+            if page.metadata.get("page",{}).get("title"):
+                metaJson["title"]=page.metadata.get("page",{}).get("title")
+            if page.metadata.get("og",{}).get("description"):
+                metaJson["description"]=page.metadata.get("og",{}).get("description")
+            if page.metadata.get("meta",{}).get("description"):
+                metaJson["description"]=page.metadata.get("meta",{}).get("description")
+        except:
+            print("error")
     return JsonResponse(metaJson,status=200)
 
 
